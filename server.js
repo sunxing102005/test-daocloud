55a5e1ddd142e34293dd48eee7a0ccd6ce07f1d2
xing.sun <xing.sun@nio.com>
2019-05-27 20:51:01 +0800

    xx

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ var PORT = 3000;
 var app = express();
 const ip = require("ip");
 app.get("/getRemoteIp", (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
     res.send({ ip: ip.address() });
 });
 app.get("/", function(req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
     res.send("Helloworld\n");
 });
 app.listen(PORT);
